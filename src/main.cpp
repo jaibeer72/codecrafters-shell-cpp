9388f740505c90fb4387cf2d40aabe99294f007f
Jaibeer Singh Dugal <jaibeersinghdugal@Jaibeers-MacBook-Pro.local>
2026-01-18 17:12:55 +0530

    fix test failure

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,7 @@
 #include <string>
 
 int main() {
-  // Flush after every std::cout / std:cerr
+  // Flush after every std::cout / std::cerr
   std::cout << std::unitbuf;
   std::cerr << std::unitbuf;
 
@@ -10,8 +10,10 @@ int main() {
 
   std::string line;
   while (std::getline(std::cin, line)) {
-    // treaating everything to be an error 
-    std::cerr << "\n" << line << ": command not found";
-
+    // treat everything as an error
+    std::cerr << line << ": command not found\n";
+    std::cout << "$ ";
   }
+
+  return 0;
 }
