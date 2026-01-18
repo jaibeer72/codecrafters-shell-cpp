08828a36efa38caf287da208c4de04a9535a70ae
Jaibeer Singh Dugal <jaibeersinghdugal@Jaibeers-MacBook-Pro.local>
2026-01-18 17:09:45 +0530

    Fix test failure

--- src/main.cpp
+++ src/main.cpp
@@ -11,8 +11,7 @@ int main() {
   std::string line;
   while (std::getline(std::cin, line)) {
     // treaating everything to be an error 
-    std::cout << "\n" << line << ": command not found" << std::endl;
-    std::cout << "$ "; 
+    std::cout << "\n" << line << ": command not found";
 
   }
 }
